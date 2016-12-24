1090baf17ce9a7dd5ec23ac177765b9e69d1efab
Kurnevsky Evgeny <kurnevsky@gmail.com>
2016-12-24 20:28:20 +0300

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+extern crate libc;
+extern crate seccomp_sys;
+extern crate clap;
+
+mod seccomp;
+
+use clap::{Arg, App, SubCommand};
+
+use libc::*;
+
+fn main() {
+  let matches = App::new("rusty-cage")
+    .version("0.1.0")
+    .author("Evgeny Kurnevsky <kurnevsky@gmail.com>")
+    .about("Command line tool")
+    .subcommand(SubCommand::with_name("seccomp")
+                .arg(Arg::with_name("whitelist")
+                     .short("w")
+                     .long("whitelist")
+                     .takes_value(true)
+                     .multiple(true))
+                .arg(Arg::with_name("blacklist")
+                     .short("b")
+                     .long("blacklist")
+                     .takes_value(true)
+                     .multiple(true)))
+    .get_matches();
+  println!("{:?}", seccomp::acticate())
+}
---
+++ src/seccomp.rs
@@ -0,0 +1,50 @@
+use libc::*;
+
+const PR_SET_SECCOMP: c_int = 22;
+const PR_SET_NO_NEW_PRIVS: c_int = 38;
+
+const SECCOMP_MODE_FILTER: c_ulong = 2;
+
+#[repr(C)]
+#[derive(Clone, Debug)]
+struct sock_filter {
+    code: u16,
+    jt: u8,
+    jf: u8,
+    k: u32,
+}
+
+#[repr(C)]
+#[derive(Clone, Debug)]
+struct sock_fprog {
+    len: c_ushort,
+    filter: *const sock_filter,
+}
+
+pub fn acticate() -> Result<(), String> {
+  let set_no_new_privs_result = unsafe {
+    prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0)
+  };
+  if set_no_new_privs_result != 0 {
+    return Err(format!("Failed to set NO_NEW_PRIVS flag with status: {}", set_no_new_privs_result));
+  }
+  let vec = vec!(
+    sock_filter {
+      code: 0,
+      jt: 0,
+      jf: 0,
+      k: 0,
+    }
+  );
+  let prog = sock_fprog {
+    len: vec.len() as c_ushort,
+    filter: vec.as_ptr(),
+  };
+  let set_seccomp_result = unsafe {
+    prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, &prog as *const sock_fprog as c_ulong, !0, 0)
+  };
+  if set_seccomp_result != 0 {
+    return Err(format!("Failed to set seccomp filter with status: {}", set_seccomp_result));
+  }
+  Ok(())
+}
