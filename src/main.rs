99847d45fc3dd5556974cafbb566840a77a67637
Kurnevsky Evgeny <kurnevsky@gmail.com>
2016-12-30 16:34:35 +0300

    Custom panic handler.

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ mod seccomp_vm;
 mod seccomp;
 mod sandbox;
 
+use std::panic;
 use std::process::Command;
 use clap::{Arg, App, ArgMatches};
 
@@ -39,6 +40,27 @@ fn parse_seccomp_params(matches: &ArgMatches) -> Option<sandbox::SeccompParams> 
   }
 }
 
+fn print_seccomp_panic_msg(seccomp_panic: &seccomp::SeccompPanic) {
+  match *seccomp_panic {
+    seccomp::SeccompPanic::SetNoNewPrivsFailed(code) => println!("Failed to set NO_NEW_PRIVS flag with status: {}", code),
+    seccomp::SeccompPanic::SetDumpableFlagFailed(code) => println!("Failed to set DUMPABLE flag with status: {}", code),
+    seccomp::SeccompPanic::SetSeccompFilterFailed(code) => println!("Failed to set seccomp filter with status: {}", code),
+    seccomp::SeccompPanic::UnknownSyscallName(ref syscall) => println!("Unknown syscall name: {}", syscall)
+  }
+}
+
+fn set_panic_hook() {
+  let default_hook = panic::take_hook();
+  panic::set_hook(Box::new(move |panic_info| {
+    let payload = panic_info.payload();
+    if let Some(seccomp_panic) = payload.downcast_ref::<seccomp::SeccompPanic>() {
+      print_seccomp_panic_msg(seccomp_panic);
+    } else {
+      default_hook(panic_info);
+    }
+  }));
+}
+
 fn main() {
   let matches = App::new("rusty-cage")
     .version("0.1.0")
@@ -60,5 +82,6 @@ fn main() {
     .get_matches();
   let seccomp_params = parse_seccomp_params(&matches);
   let mut command = build_command(&mut matches.values_of("program").expect("")); //TODO msg
+  set_panic_hook();
   sandbox::Sandbox::new(seccomp_params).run(&mut command);
 }
--- src/seccomp.rs
+++ src/seccomp.rs
@@ -10,6 +10,14 @@ const SUID_DUMP_DISABLE: c_int = 0;
 
 const SECCOMP_MODE_FILTER: c_ulong = 2;
 
+#[derive(Clone, Debug)]
+pub enum SeccompPanic {
+  SetNoNewPrivsFailed(c_int),
+  SetDumpableFlagFailed(c_int),
+  SetSeccompFilterFailed(c_int),
+  UnknownSyscallName(String)
+}
+
 #[derive(Clone, Copy, Debug)]
 pub enum SeccompFilterType {
   Whitelist,
@@ -21,7 +29,7 @@ fn set_no_new_privs() {
     prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0)
   };
   if result != 0 {
-    panic!("Failed to set NO_NEW_PRIVS flag with status: {}", result);
+    panic!(SeccompPanic::SetNoNewPrivsFailed(result));
   }
 }
 
@@ -30,7 +38,7 @@ fn set_dumpable() {
     prctl(PR_SET_DUMPABLE, SUID_DUMP_DISABLE)
   };
   if result != 0 {
-    panic!("Failed to set DUMPABLE flag with status: {}", result);
+    panic!(SeccompPanic::SetDumpableFlagFailed(result));
   }
 }
 
@@ -47,7 +55,7 @@ fn build_program(filter_type: SeccompFilterType, syscalls_list: &[String]) -> Ve
       result.push(sock_filter { code: BPF_JMP + BPF_JEQ + BPF_K, jt: 0, jf: 1, k: number });
       result.push(sock_filter { code: BPF_RET + BPF_K, jt: 0, jf: 0, k: inner_ret });
     } else {
-      panic!("Unknown syscall name: {}", syscall);
+      panic!(SeccompPanic::UnknownSyscallName(syscall.to_owned()));
     }
   }
   result.push(sock_filter { code: BPF_RET + BPF_K, jt: 0, jf: 0, k: outer_ret });
@@ -64,7 +72,7 @@ fn set_seccomp_filter(filter_type: SeccompFilterType, syscalls_list: &[String]) 
     prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, &prog as *const sock_fprog as c_ulong)
   };
   if result != 0 {
-    panic!("Failed to set seccomp filter with status: {}", result);
+    panic!(SeccompPanic::SetSeccompFilterFailed(result));
   }
 }
 
